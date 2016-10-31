29450651a51f0929c21b157cd7a8602990c060ce
Justin McCormick <justin.mccormick@blackboard.com>
2016-10-30 22:40:47 -0400

    Adding code coverage support

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var ts = require('gulp-typescript');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('test', function() {
-  return gulp.src(['dist/test/**_test.js'], {read: false})
-    .pipe(mocha({ reporter: 'spec' }));
+function getTestStream() {
+  return gulp.src(['dist/test/**_test.js'], {read: false});
+}
+
+gulp.task('coverage', function() {
+  getTestStream()
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', [ 'coverage' ], function() {
+  return getTestStream()
+    .pipe(mocha({ reporter: 'spec' }))
+    .pipe(istanbul.writeReports({
+      reporters: [ 'lcov', 'text' ],
+      dir: './dist/coverage'
+    }));
 });
 
 gulp.task('build', function() {
@@ -13,4 +28,8 @@ gulp.task('build', function() {
     .pipe(tsProject());
 
   tsResult.js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('coverage', function() {
+
 });
